fb17cf4c99f679cc755e3c6497330b7f070db3b6
MNZamir <101236319+MNZamir@users.noreply.github.com>
2022-04-27 12:03:13 +0800

    Update DeviceRepository.java

--- src/main/backend/src/main/java/com/example/java/data/repository/DeviceRepository.java
+++ src/main/backend/src/main/java/com/example/java/data/repository/DeviceRepository.java
@@ -2,6 +2,8 @@ package com.example.java.data.repository;
 
 import com.example.java.data.models.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -10,4 +12,7 @@ import java.util.Optional;
 @Repository
 @Transactional
 public interface DeviceRepository extends JpaRepository<Device, String> {
+
+    @Query(value = "SELECT * FROM Device WHERE Device.uuid = :uuid", nativeQuery = true)
+    Device findByUuid(@Param("uuid") String uuid);
 }
