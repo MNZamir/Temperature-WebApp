da8ff1c5d462524093576c3a22b87ec7a056ec1c
MNZamir <101236319+MNZamir@users.noreply.github.com>
2022-04-27 09:47:10 +0800

    Update FileRepository.java

--- src/main/backend/src/main/java/com/example/java/data/repository/FileRepository.java
+++ src/main/backend/src/main/java/com/example/java/data/repository/FileRepository.java
@@ -4,6 +4,9 @@ import com.example.java.data.models.FileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface FileRepository extends JpaRepository<FileEntity, String> {
 }
